708c3cd9dd2238354912150b87e1c624e8b4e70c
shubhampal805731-netizen <shubhampal805731@gmail.com>
2026-04-20 13:24:55 +0530

    Refactor environment variable usage for bot token

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import os
 
 app = Flask(__name__)
 
-BOT_TOKEN = os.getenv("8704983034:AAEGPn9P-YKIOxuVGiNAw_M0zisU9yh9fT8")
+# ENV VARIABLES
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
 @app.route('/')
@@ -16,12 +17,19 @@ def postback():
     user_id = request.args.get('user_id')
     amount = request.args.get('amount', '0')
 
+    # MESSAGE FORMAT
     text = f"{user_id}:IN\n{user_id}:IN:{amount}"
 
+    # TELEGRAM SEND
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    requests.post(url, data={"chat_id": CHAT_ID, "text": text})
+    
+    requests.get(url, params={
+        "chat_id": CHAT_ID,
+        "text": text
+    })
 
     return "OK"
 
+# RUN APP
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
